ad1cc6133ef6a9fcbf6fd952e8a7e7f50a424b88
shambashib20 <shambashibmajumdar2000@gmail.com>
2025-01-06 10:46:41 +0530

    started the models

---
+++ models/User.ts
@@ -0,0 +1,46 @@
+import mongoose, { Schema, model, models, Types } from "mongoose";
+import bcrypt from "bcryptjs";
+import { unique } from "next/dist/build/utils";
+
+export interface IUser {
+  email: string;
+  password: string;
+  role: "user" | "admin";
+  _id?: Types.ObjectId;
+  createdAt?: Date;
+  updatedAt?: Date;
+}
+
+const userSchema = new Schema<IUser>(
+  {
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    role: {
+      type: String,
+      enum: ["user", "admin"],
+      default: "user",
+    },
+  },
+
+  {
+    timestamps: true,
+    versionKey: false,
+  }
+);
+
+userSchema.pre("save", async function () {
+  if (this.isModified("password")) {
+    this.password = await bcrypt.hash(this.password, 10);
+  }
+});
+
+const User = models?.User || model<IUser>("User", userSchema);
+
+export default User;
